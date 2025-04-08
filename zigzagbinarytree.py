a1e87544dac2e1fa87451d4bf51eda2b50143100
Nguyễn Lưu Phương Uyên <luuphuonguyen04@gmail.com>
2025-04-08 09:31:50 +0700

    [Ex6] Update correction solution and add comment

--- zigzagbinarytree.py
+++ zigzagbinarytree.py
@@ -1,17 +1,18 @@
 def pathInZigZagTree(label):
-        result = []
-        level = 0
-        node_count = 1
-        while node_count * 2 <= label:
-            node_count *= 2
-            level += 1
-        while label >= 1:
+        result=[]
+        level=0
+        node_count=1
+        #calculate level of the label
+        while node_count*2<= label:
+            node_count*= 2
+            level=level+1
+        while label >=1:
             result.append(label)
-            level_start = 2 ** level
-            level_end = 2 ** (level + 1) - 1
-            label = (level_start + level_end - label) // 2
-            level -= 1
-        return result[::-1]
+            level_start=2**level #min num of the current level
+            level_end=2**(level+1)-1 #max num of the current level
+            label=(level_start+level_end-label)//2 #find the parent node
+            level=level-1 #up we go to the parent level
+        return result[::-1] #reverse the array
 
 print(pathInZigZagTree(14))  # Output: [1, 3, 4, 14]
 print(pathInZigZagTree(26))  # Output: [1, 2, 6, 10, 26]
