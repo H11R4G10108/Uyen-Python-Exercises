0765f97d83ddcd1b8629a230ccbaddeca244a856
Nguyễn Lưu Phương Uyên <luuphuonguyen04@gmail.com>
2025-04-09 10:20:16 +0700

    [Ex4] Update more optimized solution

--- dayoftheyear.py
+++ dayoftheyear.py
@@ -1,7 +1,9 @@
-#Description: find the day of the year for the given date
-#Input: A date string
-#Output: interger representing the day of the year
+"""Description: find the day of the year for the given date
+Input: A date string
+Output: interger representing the day of the year
+"""
 
+"""Ver1"""
 def dayOfYear(date):
         hashmap={
             1:31,
@@ -32,10 +34,24 @@ def dayOfYear(date):
                 dayneeded=dayneeded+int(hashmap[i])
         return dayneeded
 
-# Example usage:
 date = "2019-01-09"
 print(dayOfYear(date))  # Output: 9
 date22 = "2019-02-10"
 print(dayOfYear(date22))  # Output: 41
 date23 = "2019-03-01"
 print(dayOfYear(date23))  # Output: 60
+
+"""Ver2"""
+def dayOfYear2(date):
+    year, month, day = map(int, date.split('-'))
+    months = [0, 31, 59 ,90, 120, 151, 181, 212, 243, 273, 304, 334] #list of days in a year when a specific month ends
+    if month > 2 and (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)): #check leaps year only when the date is past jan
+        day+=1
+    return months[month - 1] + day
+
+date1 = "2019-01-09"
+print(dayOfYear2(date1))  # Output: 9
+date22 = "2019-02-10"
+print(dayOfYear2(date22))  # Output: 41
+date23 = "2019-03-01"
+print(dayOfYear2(date23))  # Output: 60
